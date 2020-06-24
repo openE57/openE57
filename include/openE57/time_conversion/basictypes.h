79e16a9e1c6df0cfb236d3bd89e38eb43b3927e6
Michele Adduci <adduci.michele@gmail.com>
2020-06-24 05:40:03 +0200

    Applied clang-format to time_conversion

--- include/openE57/time_conversion/basictypes.h
+++ include/openE57/time_conversion/basictypes.h
@@ -17,20 +17,20 @@ modification, are permitted provided the following conditions are met: \n
 - Redistributions in binary form must reproduce the above copyright
   notice, this list of conditions and the following disclaimer in the
   documentation and/or other materials provided with the distribution. \n
-- The name(s) of the contributor(s) may not be used to endorse or promote 
-  products derived from this software without specific prior written 
+- The name(s) of the contributor(s) may not be used to endorse or promote
+  products derived from this software without specific prior written
   permission. \n
 
-THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS ``AS IS'' AND ANY EXPRESS 
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
 INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
-OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
 */
 
@@ -38,20 +38,21 @@ SUCH DAMAGE.
 #define _C_BASICTYPES_H_
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/// The 'c' boolean type is a define for (int). TRUE(0), FALSE(1).
-typedef int BOOL;
+  /// The 'c' boolean type is a define for (int). TRUE(0), FALSE(1).
+  typedef int BOOL;
 
 #ifndef FALSE
-#define FALSE (0)
+#  define FALSE (0)
 #endif
 
 #ifndef TRUE
-#define TRUE (1)
+#  define TRUE (1)
 #endif
-  
+
 #ifdef __cplusplus
 }
 #endif
