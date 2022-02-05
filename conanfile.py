d23fdaf1504ff7def38b7660d4b89eb9fd20257d
Michele Adduci <adduci.michele@gmail.com>
2022-02-05 05:12:16 +0000

    Initial conanfile

---
+++ conanfile.py
@@ -0,0 +1,93 @@
+from conans import ConanFile, CMake, tools
+import os, shutil
+
+
+class ConanFileDefault(ConanFile):
+    name = "openE57"
+    description = "A C++ library for reading and writing E57 files, " \
+                  "fork of the original libE57 (http://libe57.org)"
+    topics = ("conan", "openE57", "e57")
+    version = "1.51.1"
+    url = "https://github.com/openE57/openE57"
+    homepage = "https://github.com/openE57/openE57"
+    license = ("MIT", "E57 Software Licenses")
+    exports_sources = ["CMakeLists.txt", "cmake/*.cmake", "LICENSE*", "*.md", "*.txt"]
+    generators = "cmake", "cmake_find_package"
+    short_paths = True
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"with_examples": [True, False],
+               "with_tools": [True, False],
+               "mt": [True, False],
+               "shared": [True, False]}
+
+    default_options = {
+        'with_examples': False,
+        'with_tools': False,
+        'with_tests': True,
+        'mt': False,
+        'shared': False}
+
+    def validate(self):
+        if sself.options.shared == True:
+            raise ConanInvalidConfiguration("OpenE57 cannot be built as shared library yet")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def build_requirements(self):
+        if tools.cross_building(self, skip_x64_x86=True) and hasattr(self, 'settings_build'):
+            self.build_requires("openE57/{}".format(self.version))
+
+        if self.options.with_tools == True:
+            self.build_requires("boost/1.77.0")
+            self.options["boost"].multithreading = True
+            self.options["boost"].shared = True
+
+    def requirements(self):
+        self.requires("icu/70.1")
+        self.options["icu"].shared = False
+
+        self.requires("xerces-c/3.2.3")
+        self.options["xerces-c"].shared = False
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.definitions["BUILD_EXAMPLES"] = self.options.with_examples
+        self._cmake.definitions["BUILD_TOOLS"] = self.options.with_tools
+        self._cmake.definitions["BUILD_TESTS"] = self.options.with_tests
+        self._cmake.definitions["BUILD_WITH_MT"] = self.options.mt
+        self._cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def package_info(self):
+        defines = []
+        self.cpp_info.libs = ["openE57"]
+
+        if self.options.mt:
+            defines.append("BUILD_WITH_MT")
+        if self.options.with_cookies:
+        self.cpp_info.defines = defines
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE.libE57", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        shutil.move(os.path.join(self.package_folder, 'import'), os.path.join(self.package_folder, 'bin', 'import'))
