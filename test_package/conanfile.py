bae1059e396f9ab3835558a22e83f2dab3669a90
Michele Adduci <adduci.michele@gmail.com>
2022-02-12 05:59:42 +0000

    Improving local conan recipe, debugging issues with MSVC linking

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,15 @@
 import shutil,os
+from conan.tools.microsoft import msvc_runtime_flag
 from conans import ConanFile, CMake, tools
 
-class TestOpenE57Conan(ConanFile):
+class TestOpene57Conan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package"
 
     def build(self):
         if not tools.cross_building(self):
             cmake = CMake(self)
+            cmake.definitions["BUILD_WITH_MT"] = "MT" in str(msvc_runtime_flag(self))
             cmake.configure()
             cmake.build()
 
@@ -15,7 +17,7 @@ class TestOpenE57Conan(ConanFile):
         if not tools.cross_building(self):
             bin_path=""
             if self.settings.compiler == "Visual Studio":
-                bin_path = os.path.join("bin", "{}".format(self.settings.build_type), "openE57_example")
+                bin_path = os.path.join("bin", "{}".format(self.settings.build_type), "opene57_example")
             else:
-                bin_path = os.path.join("bin", "openE57_example")
+                bin_path = os.path.join("bin", "opene57_example")
             self.run(bin_path, run_environment=True)
