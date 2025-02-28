babe50df4beb7bd2b2b2c71316bb11e2772b843d
oleksii <lexa562@ukr.net>
2025-02-28 10:47:33 +0200

    get xerces-c library through fetch content if it's not found

--- src/cmake/dependencies.cmake
+++ src/cmake/dependencies.cmake
@@ -1,22 +1,37 @@
+include(FetchContent)
 # Find the Xerces libraries
 set(Xerces_USE_STATIC_LIBS ON)
-find_package(XercesC 3.2 REQUIRED)
-set(XML_LIBRARIES ${XercesC_LIBRARIES})
-set(XML_INCLUDE_DIRS ${XercesC_INCLUDE_DIRS})
+find_package(XercesC 3.2 QUIET)
+if (XercesC_FOUND)
+  set(XML_LIBRARIES ${XercesC_LIBRARIES})
+  set(XML_INCLUDE_DIRS ${XercesC_INCLUDE_DIRS})
+  # Add ICU in Linux Systems
+  if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR ${CMAKE_SYSTEM_NAME} STREQUAL "Apple")
+    find_package(ICU REQUIRED)
+    set(XML_LIBRARIES ${XML_LIBRARIES} ${ICU_LIBRARIES})
+    set(XML_INCLUDE_DIRS ${XML_INCLUDE_DIRS} ${ICU_INCLUDE_DIRS})
+  endif()
+else()
+  if (NOT TARGET xerces-c)
+    set(XERCES_C_REPO https://github.com/apache/xerces-c.git)
+    FetchContent_Declare(
+      xerces-c
+      EXCLUDE_FROM_ALL
+      GIT_REPOSITORY ${XERCES_C_REPO}
+      GIT_TAG v3.3.0
+      GIT_SHALLOW TRUE
+    )
+    FetchContent_MakeAvailable(xerces-c)
+  endif()
+  set(XML_LIBRARIES "xerces-c")
+  get_target_property(XercesC_INCLUDE_DIRS xerces-c INCLUDE_DIRECTORIES)
+  set(XML_INCLUDE_DIRS ${XercesC_INCLUDE_DIRS})
+endif()
+
 if(NOT BUILD_SHARED_LIBS)
   list(APPEND compiler_definitions XERCES_STATIC_LIBRARY)
 endif()
   
-# Add ICU in Linux Systems
-if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" OR ${CMAKE_SYSTEM_NAME} STREQUAL "Apple")
-  list(APPEND compiler_definitions LINUX)
-  find_package(ICU REQUIRED)
-  set(XML_LIBRARIES ${XML_LIBRARIES} ${ICU_LIBRARIES})
-  set(XML_INCLUDE_DIRS ${XML_INCLUDE_DIRS} ${ICU_INCLUDE_DIRS})
-elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-  list(APPEND compiler_definitions WINDOWS)
-endif()
-
 # Find Boost (Required by Tools)
 if(BUILD_TOOLS)
   if(NOT BUILD_SHARED_LIBS) 
